e4320fc977e28e13248cb1ae0135b2ef813baca7
IsaacLake03 <156034639+IsaacLake03@users.noreply.github.com>
2024-03-02 15:15:02 -0800

    Got the text box out of header
    Into main area

--- lettergen/src/App.js
+++ lettergen/src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
-/* <meta name ="viewport" content="width = device-width, initial-scale 
-= 1"> */
 
-{/* <link rel="stylesheet" type="text/css" href=""> */}
-
-
-function App () {
+function App() {
   return (
     <div className="Banner">
-        <h1>Letter Gen</h1>
+      <h1>Letter Gen</h1>
+
+      <div className="bottomSection">
+        <div className="textbox-container">
+          <textarea className="textbox" placeholder="Tell us what happened"></textarea>
+        </div>
+        
+        <div>
+          <button className="bottomButton btn-success btn-lg">Review Case</button>
+        </div>
+      </div>
     </div>
   );
 }
-
 
 export default App;
