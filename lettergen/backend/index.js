775c6fef937df04e08b259d42bb591a9e3d598f5
Wyatt Tack <wtack@calpoly.edu>
2024-03-02 15:44:44 -0800

    kms

---
+++ lettergen/backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+      })
+      .then(console.log("MongoDB Connected"))
+      .catch((err) => console.log(err));
+
+app.use("/", (req, res) => 
+{
+    console.log("this is the home")
+})
+app.listen(process.env.PORT,() => {
+    console.log("Backend is running.");
+});
+
+
