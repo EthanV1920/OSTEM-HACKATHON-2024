8a14f6be1cce4374932ecfebeb184b92d218aa8c
Ethan Vosburg <ethan@vosburg.us>
2024-03-02 20:18:34 -0800

    Getting information from the form entries

--- lettergen/src/App.js
+++ lettergen/src/App.js
@@ -2,24 +2,40 @@ import React from 'react';
 import './App.css';
 import './askChatGPT.js'
 
-class App extends React.Component{
-    askChatGPT = () => {
+function App(){
+    function askChatGPT() {
         console.log("Ask Chat Triggered")
     }
-    render(){
-        return(
+    function handleSubmit(e) {
+        // Prevent reload
+        e.preventDefault();
+
+        // Read data from the form
+        const form = e.target;
+        const formData = new FormData(form);
+
+        // Console log data
+        const formDataConsole = Object.fromEntries(formData.entries(0));
+        console.log(formDataConsole);
+    }
+    return(
+        <form method='post' onSubmit={handleSubmit}>
             <div className="Banner">
-            <h1>Letter Gen</h1>
+            <h1 name="Form Name">Letter Gen</h1>
             <div className="bottomSection">
                 <div className="textbox-container" style={{ paddingBottom: '30px' }}>
-                    <textarea className="textbox" placeholder="Describe your Situation"></textarea>
+                    <textarea 
+                    name="data" 
+                    id="chatBox" 
+                    className="textbox" 
+                    placeholder="Describe your Situation"></textarea>
                 </div>
                 <div>
-                    <button className="bottomButton btn-danger btn-lg" onClick={this.askChatGPT}>Review Case</button>
+                    <button className="bottomButton btn-danger btn-lg" onClick={askChatGPT}>Review Case</button>
                 </div>
             </div>
-        </div>
-        )
-    }
+            </div>
+        </form>
+    );
 }
 export default App;
