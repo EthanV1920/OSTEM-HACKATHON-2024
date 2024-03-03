e8ec9f5c89809239eb413573a0b30f95907314bb
IsaacLake03 <156034639+IsaacLake03@users.noreply.github.com>
2024-03-03 14:08:59 -0800

    form now linked

--- lettergen/src/App.js
+++ lettergen/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 
 function App() {
+  const handleDownloadForm = () => {
+    // Replace the link with your desired URL
+    window.open('https://elections.cdn.sos.ca.gov/fraud-complaints/pdfs/english-voter-fraud-complaint-form.pdf', '_blank');
+  };
+
   return (
     <div className="Banner">
       <h1>Voter Advocate</h1>
@@ -12,7 +17,7 @@ function App() {
         
         <div>
           <button className="bottomButton btn-lg">Review Case</button>
-          <button className="Form btn-lg">Download Form</button>
+          <button className="Form btn-lg" onClick={handleDownloadForm}>Download Form</button>
         </div>
       </div>
     </div>
