62578cffbdaa6b2ce8646c628a3cfce591c0ef5a
Wyatt Tack <wtack@calpoly.edu>
2024-03-03 10:58:17 -0800

    VON!

---
+++ lettergen/src/FAQ.js
@@ -0,0 +1,32 @@
+import './FAQ.css';
+
+function App() {
+  return (
+    <div className="Banner">
+      <h1>FAQ</h1>
+      <h2 class = "topText">And information on your voting rights</h2>
+        <div className="QuestionText">
+
+What are some common forms of voter disenfranchisement?
+</div><div className="Text">
+Common forms include voter ID laws, restrictive voter registration requirements, gerrymandering, felony disenfranchisement laws, limited access to polling stations, and misinformation campaigns.
+</div><div className="QuestionText">
+How does voter disenfranchisement affect democracy?
+</div><div className="Text">
+Voter disenfranchisement undermines the principles of democracy by restricting the participation of certain groups, leading to unequal representation and potentially skewing election outcomes.
+</div><div className="QuestionText">
+What are some historical examples of voter disenfranchisement?
+</div><div className="Text">
+Historical examples include poll taxes, literacy tests, grandfather clauses, and violence and intimidation targeted at suppressing the voting rights of African Americans and other marginalized groups.
+</div><div className="QuestionText">
+What groups are most affected by voter disenfranchisement?
+</div><div className="Text">
+Marginalized groups such as racial minorities, low-income individuals, young people, and individuals with disabilities are disproportionately affected by voter disenfranchisement.
+</div>
+
+
+    </div>
+  );
+}
+
+export default App;
