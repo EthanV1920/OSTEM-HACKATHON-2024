f4343221671d86d9bb67af7f2704225308a6d9f7
IsaacLake03 <156034639+IsaacLake03@users.noreply.github.com>
2024-03-02 17:58:35 -0800

    Got some stuff done
    Here we go

---
+++ lettergen/src/About.js
@@ -0,0 +1,24 @@
+import './About.css';
+
+function About() {
+  return (
+    <div className="Banner">
+        <div></div>
+      <h1>About</h1>
+      <p class = "topText">AI Voting Right Bot</p>
+        <div className="Text">
+          <p>
+          Disenfranchisement is unfortunately becoming a prevalent issue in today's caustic political environment. Given the precarious state of our democracy, it is imperative that as many people as possible participate in elections. A crucial aspect of achieving this is ensuring the safety, security, and peace of mind of our voters.
+
+          Legalese, an unnecessarily complicated language, is designed to prevent people from being informed about their rights and the laws governing our processes. Our app, equipped with an encyclopedic knowledge of voting suppression laws in California, interprets any potential situation you may encounter while voting and determines its legality. If the action is deemed illegal, the app assists in guiding you through the process of submitting a formal complaint to the government regarding the transgression. In more serious cases, it alerts you to contact a lawyer.
+
+          The first step in addressing injustice is recognizing it, and our tool is designed to aid in that process.
+
+          </p>
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
