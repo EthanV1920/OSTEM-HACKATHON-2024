0ebb6c37a122ae5b53a8bd3ed535745df13c00e8
Sam Solano <slsolano@calpoly.edu>
2024-03-02 16:13:14 -0800

    made banner centered

--- lettergen/src/App.js
+++ lettergen/src/App.js
@@ -5,13 +5,23 @@ import './App.css';
 {/* <link rel="stylesheet" type="text/css" href=""> */}
 
 
-function App () {
+
+function App() {
   return (
     <div className="Banner">
-        <h1>Letter Gen</h1>
+      <h1>Letter Gen</h1>
+
+      <div className="bottomSection">
+        <div className="textbox-container">
+          <textarea className="textbox" placeholder="Tell us what happened"></textarea>
+        </div>
+          <p class = "topText">AI Voting Right Bot</p>
+        <div>
+          <button className="bottomButton btn-danger btn-lg">Review Case</button>
+        </div>
+      </div>
     </div>
   );
 }
-
 
 export default App;
